990385cf0db097cdc985971c1849ab4fdac516eb
Himanshu <himanshupotpose388@gmail.com>
2026-02-21 16:41:50 +0530

    commit

--- ec2.tf
+++ ec2.tf
@@ -7,18 +7,9 @@ resource "aws_instance" "apex" {
     ami = "ami-019715e0d74f695be"
     instance_type = "t3.micro"
     key_name = "hp-key"
-    vpc_security_group_ids = [data.aws_default_security_group.default.id]
+    vpc_security_group_ids = ["sg-03b15ce3f19883c15"]
     tags = {
       name = "my_instance"
       env = "dev"
     }
-}
-
-data "aws_vpc" "default" {
-    default = true
-  
-}
-
-resource "aws_default_security_group" "default" {
-    vpc_id = data.aws_vpc.default.id
 }
